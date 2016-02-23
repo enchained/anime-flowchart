dee7c76762074863803243b29e84b673947dafa4
enchained <enchained@users.noreply.github.com>
2016-02-23 06:59:25 +0300

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,127 @@
+var SHORT = {
+  "first": {
+    "text": "Looking for your first anime?",
+    "choices": {
+      "epic": {
+        "desc": "**Something epic**  \nlike Avatar: The Last Airbender",
+        "id": 5114,
+        "name": "Fullmetal Alchemist: Brotherhood"
+      },
+      "western": {
+        "desc": "**Space western**  \n(Firefly, Guardians of the Galaxy)",
+        "id": 1,
+        "name": "Cowboy Bebop"
+      },
+      "crime": {
+        "desc": "**Crime drama mindgames**  \nwith the Grim Reaper",
+        "id": 1535,
+        "name": "Death Note"
+      }
+    },
+    "prev": "start",
+    "next": {
+      "text": "Seen these 3 already (or need something different)",
+      "category": "laugh"
+    }
+  },
+  "laugh": {
+    "text": "Need a good laugh?",
+    "choices": {
+      "action": {
+        "desc": "**Popular action comedy**  \n(with drama, \"feels\")",
+        "id": 6547,
+        "name": "Angel Beats!"
+      },
+      "romance": {
+        "desc": "**Popular romance comedy**  \n(evolves into drama)",
+        "id": 4224,
+        "name": "Toradora!"
+      },
+      "workplace": {
+        "desc": "**Popular workplace comedy**  \n(no drama!)",
+        "id": 15809,
+        "name": "Hataraku Maou-sama! (The Devil is a Part-Timer!)"
+      }
+    },
+    "prev": "first",
+    "next": {
+      "text": "Let's stick to the action-adventure",
+      "category": "serious"
+    }
+  },
+  "serious": {
+    "text": "Something dark and serious, then?",
+    "choices": {
+      "cyberpunk": {
+        "desc": "**Psychological** police  \nin a **cyberpunk** setting",
+        "id": 13601,
+        "name": "Psycho-Pass"
+      },
+      "superpowers": {
+        "desc": "**Superpowers, X-Men style**  \n(Chinese Electric Batman)",
+        "id": 2025,
+        "name": "Darker than Black"
+      },
+      "timetravel": {
+        "desc": "**Time travel experiment**  \nthriller",
+        "id": 9253,
+        "name": "Steins;Gate"
+      }
+    },
+    "prev": "laugh",
+    "next": {
+      "text": "Don't overdo it",
+      "category": "japanese"
+    }
+  },
+  "japanese": {
+    "text": "Want something typically Japanese?",
+    "choices": {
+      "samurai": {
+        "desc": "**Samurai** are cool  \n(even cooler with hip-hop music)",
+        "id": 205,
+        "name": "Samurai Champloo"
+      },
+      "robots": {
+        "desc": "I'VE HEARD THERE ARE  \nSOME **GIANT ROBOTS**",
+        "id": 2001,
+        "name": "Tengen Toppa Gurren Lagann"
+      },
+      "spirits": {
+        "desc": "I like the Ghibli movies,  \nespecially the **spirit stuff**",
+        "id": 457,
+        "name": "Mushishi"
+      }
+    },
+    "prev": "serious",
+    "next": {
+      "text": "Something more Western, please",
+      "category": "fantasy"
+    }
+  },
+  "fantasy": {
+    "text": "Fantasy action adventure?",
+    "choices": {
+      "gritty": {
+        "desc": "**Something gritty, edgy**  \nand with epic music",
+        "id": 16498,
+        "name": "Shingeki no Kyojin (Attack on Titan)"
+      },
+      "arabian": {
+        "desc": "**Arabian Nights** road trip adventure,  \nAvatar-style",
+        "id": 14513,
+        "name": "Magi: The Labyrinth of Magic"
+      },
+      "pirates": {
+        "desc": "**Pirates of the Caribbean** meets **Diablo**",
+        "id": 21843,
+        "name": "Shingeki no Bahamut: Genesis"
+      }
+    },
+    "prev": "japanese",
+    "next": {
+      "text": "I want more!",
+      "category": "extended"
+    }
+  }
+}
